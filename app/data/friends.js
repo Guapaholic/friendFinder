c38fb29a540e0f82ade35894ead2cab6fb896587
Francis <juliancruz1892@gmail.com>
2018-10-16 14:58:39 -0400

    almost done

---
+++ app/data/friends.js
@@ -0,0 +1,9 @@
+var friends = [
+  { "name": "Rene", "photo": "http://bluewingconsulting.com/wp-content/uploads/2017/02/larry-profile-800x800.jpeg", "scores": ["5", "1", "4", "4", "5", "1", "1", "5", "1", "1"] }, 
+  { "name": "Jessica", "photo": "https://capturedtracks.com/files/Molly-Thumbnail-Website-800x800.jpg", "scores": ["4", "2", "5", "1", "3", "2", "2", "1", "3", "4"] }, 
+  { "name": "George", "photo": "http://heritagelandbank.com/sites/default/files/styles/social_thumb/public/news_events/Tiner%2C%20Ardy%20-%20No%20Hat.JPG?itok=7xjBJQiC", "scores": ["5", "5", "2", "2", "4", "1", "3", "2", "5", "5"] }, 
+  { "name": "Sherry", "photo": "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcR-6lviFvyZ1Ea9-7PyZlsNkSGtHQytRJQU5ITnyPRDN0_quxAh8g", "scores": ["3", "3", "4", "2", "2", "1", "3", "2", "2", "3"] }, 
+  { "name": "Daniel", "photo": "http://cdn.unifr.ch/ami-db/images/persons/photo/e33d0d8d-b7f8-4e9b-955d-b42e7301dc5c/square_Mayer_portrait.jpg", "scores": ["4", "3", "4", "1", "2", "2", "5", "3", "1", "4"] }, 
+  { "name": "Pedro", "photo": "https://www.drew.edu/stories/wp-content/uploads/sites/44/2018/06/fabioferraz-800x800.jpg", "scores": ["4", "2", "2", "1", "2", "2", "4", "2", "4", "5"] }];
+
+module.exports = friends;
---
+++ app/routing/apiRoutes.js
@@ -0,0 +1,32 @@
+const friends = require('../data/friends.js');
+module.exports = function (app) {
+  app.get('/api/friends', function (req, res) {
+    res.json(friends);
+  });
+  app.post('/api/friends', function (req, res) {
+    var delta = 40;
+    var matchName = '';
+    var matchPic = '';
+    friends.forEach(function (friend) {
+      var matchedScoresArray = [];
+      var totalDelta = 40;
+      function add(total, num) {
+        return total + num;
+      }
+      for (var i = 0; i < friend.scores.length; i++) {
+        matchedScoresArray.push(Math.abs(parseInt(req.body.scores[i]) - parseInt(friend.scores[i])));
+      }
+      totalDelta = matchedScoresArray.reduce(add, 0);
+      if (totalDelta < delta) {
+        delta = totalDelta;
+        matchName = friend.name;
+        matchPic = friend.photo;
+      }
+    });
+    res.json({
+      name: matchName,
+      photo: matchPic
+    });
+    friends.push(req.body);
+  });
+}
